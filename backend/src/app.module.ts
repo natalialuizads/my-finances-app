42b4b99229dc672e46f2b05a42748070f0c0d127
Natália Luiza <natalialuizads@gmail.com>
2023-09-16 13:43:07 -0300

    removendo arquivos que nao estao sendo utilizados

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ContaModule } from './conta/conta.module';
 
 @Module({
   imports: [ContaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule { }
