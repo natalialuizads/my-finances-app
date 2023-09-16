e4ba79e62aebb119da0020f4951f2cbdee49f5d2
Natália Luiza <natalialuizads@gmail.com>
2023-09-16 18:25:59 -0300

    estrutura basica do front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { TuiAppBarModule } from '@taiga-ui/addon-mobile';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -5,10 +8,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiAppBarModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
-export class AppComponent {
-  title = 'boilerplate-angular';
-}
+export class AppComponent {}
